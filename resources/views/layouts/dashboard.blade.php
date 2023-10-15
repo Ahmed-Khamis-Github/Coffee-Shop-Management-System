123f2bd853a490aef3087c7c086278197b618143
Ahmed <ahmed.samir00979@gmail.com>
2023-10-15 03:50:28 +0300

    adding offline orders for admin dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -272,9 +272,9 @@
 										</a>
 									</li>
 									<li class="nav-item">
-										<a href="./index2.html" class="nav-link">
+										<a href="{{ route('offline.index') }}" class="nav-link">
 											<i class="far fa-circle nav-icon"></i>
-											<p>Offline Orders</p>
+											<p>Offline Orders</p>	
 										</a>
 									</li>
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Dashboard\CategoryController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Dashboard\OfflineOrdersController;
 
 
  
@@ -17,7 +18,9 @@ use Illuminate\Support\Facades\Route;
     Route::resource('users',CategoryController::class) ;
    
     
- 
+    Route::prefix('orders')->group(function () {
+        Route::resource('offline', OfflineOrdersController::class);
+    });
 
 
 
