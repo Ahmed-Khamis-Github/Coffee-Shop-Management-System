349e9b5d4430c0a1da16b86e9500cafa2990f091
Ahmed <ahmed.samir00979@gmail.com>
2023-10-17 02:53:29 +0300

    adding social media authentication

--- routes/front.php
+++ routes/front.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Front\CartController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Front\ProductListController;
 
-Route::get('/', [ProductListController::class , 'index']);
+Route::get('/', [ProductListController::class , 'index'])->name('front.home');
 Route::get('checkout',function(){
 	return view('front.checkout');
 } );
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\SocialLoginController;
 use App\Http\Controllers\Dashboard\CheckoutController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Dashboard\UserController;
@@ -18,3 +19,7 @@ require __DIR__.'/auth.php';
 
 
 Route::resource('checkouts', CheckoutController::class);
+
+Route::get('auth/{provider}/redirect', [SocialLoginController::class,'redirect'])->name('auth.socialite.redirect');
+
+Route::get('auth/{provider}/callback', [SocialLoginController::class,'callback'])->name('auth.socialite.callback');
